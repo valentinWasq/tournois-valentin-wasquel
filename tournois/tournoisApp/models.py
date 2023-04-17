7d72b81cb38483fc91e0f4f7ff2af4a2b6ca3b78
valentinWasq <valentin.wasquel@student.isae-supaero.fr>
2023-04-17 17:00:37 +0200

    add models

--- tournois/tournoisApp/models.py
+++ tournois/tournoisApp/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+    Name = models.CharField(max_length=50)
+    Coach = models.CharField(max_length=50)
+    ListOfPLayer = models.CharField(max_length=200)
+
+    def toString(self):
+        return f"{self.Name} : coach: {self.Coach}; players: {self.ListOfPLayer}"
+    
+    def Match_set(self):
+        return self.Matchs_team1.all() + self.Matchs_team2.all()
+    
+    def getScore(self):
+        score = 0
+        for match in self.Matchs_team1.all():
+            score += match.Score1
+        for match in self.Matchs_team2.all():
+            score += match.Score1
+        return score
+
+class Pool(models.Model):
+    #Tournois = models.ForeignKey(Tournois, on_delete=models.DO_NOTHING)
+    index = models.IntegerField(default=-1)
+    
+    def getTeams(self):
+        AllMatch = self.match_set.all()
+        AllTeams = []
+        for match in AllMatch:
+            AllTeams += match.Teams()
+        return AllTeams
+
+
+class Match(models.Model):
+    Date = models.DateField()
+    Location = models.CharField(max_length=50)
+    Team1 = models.ForeignKey(Team, on_delete=models.DO_NOTHING, related_name="Matchs_team1")
+    Team2 = models.ForeignKey(Team, on_delete=models.DO_NOTHING, related_name="Matchs_team2")
+    Score1 = models.IntegerField(default=0)
+    Score2 = models.IntegerField(default=0)
+    Pool = models.ForeignKey(Pool, on_delete=models.DO_NOTHING)
+
+    def getScoreString(self):
+        return str(self.Score1) + ':' + str(self.Score2)
+    
+    def Teams(self):
+        return [self.Team1, self.Team2]
+    
+    def Scores(self):
+        return [self.Score1, self.Score2]
+
+    def toString(self):
+        return f"match {self.Equipe1} against {self.Equipe2}, score : {self.getScoreString()} \nat : {self.Location}, {self.Date}"
+
+class Tournament(models.Model):
+    Name = models.CharField(max_length=50)
+    Location = models.CharField(max_length=50, null=True)
+    Dates = models.CharField(max_length=200, null=True)
+    NBPool = models.IntegerField(default=0)
+    NBTeamPerPool = models.IntegerField(default = 0)
