9ec998aea363f52d6b09455238b97a2a994a943a
valentinWasq <valentin.wasquel@student.isae-supaero.fr>
2023-04-17 16:59:40 +0200

    fix issue in settings.py

--- tournois/tournois/settings.py
+++ tournois/tournois/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'tournoisApp.apps.tournoisAppConfig'
+    'tournoisApp.apps.TournoisappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
