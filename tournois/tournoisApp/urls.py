411582f19bdc94e7d6e57330027657366f601248
valentinWasq <valentin.wasquel@student.isae-supaero.fr>
2023-04-19 08:51:42 +0200

    add tournamentList view

--- tournois/tournoisApp/urls.py
+++ tournois/tournoisApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'tournois'
+app_name = 'tournament'
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('tournament/', views.tournamentList, name='tournamentList')
 ]
--- tournois/tournoisApp/views.py
+++ tournois/tournoisApp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from .models import Tournament
+
 # Create your views here.
 def home(request):
     template_name = 'tournois/Home.html'
     return render(request, template_name)
+
+def tournamentList(request):
+    template_name = 'tournois/TournamentList.html'
+    allTournament = Tournament.objects.all()
+    context = {'allTournament' : allTournament}
+    return render(request, template_name, context)
