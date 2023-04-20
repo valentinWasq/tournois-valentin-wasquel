68bd40ee19272c55297c8a8f234a8cdcf07a3d31
valentinWasq <valentin.wasquel@student.isae-supaero.fr>
2023-04-20 15:11:25 +0200

    add all models in the admin page

--- tournois/tournoisApp/admin.py
+++ tournois/tournoisApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Tournament, Pool, Match, Team, Comment
+
+admin.site.register(Tournament)
+admin.site.register(Pool)
+admin.site.register(Match)
+admin.site.register(Team)
+admin.site.register(Comment)
