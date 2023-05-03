cf747a0f6a20930684da521e3b319257319cefa3
grodcas <119622068+grodcas@users.noreply.github.com>
2023-05-03 08:16:21 +0200

    css de poules

--- tournois/tournois/settings.py
+++ tournois/tournois/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATICFILES_DIRS = [ BASE_DIR / 'static']
 STATIC_URL = 'static/'
 
 # Default primary key field type
